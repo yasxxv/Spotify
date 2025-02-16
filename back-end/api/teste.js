2be5f008ecad7c524f9048d7d2dd9b33be7f05a7
Yasmin Benicio <79879086+yasxxv@users.noreply.github.com>
2025-02-16 16:42:15 -0300

    ajuste no process env

--- back-end/api/teste.js
+++ back-end/api/teste.js
@@ -1,4 +1,1 @@
-import path from "path"
 
-const __dirname = path.resolve();
-console.log(__dirname)
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,8 +2,9 @@
 import "dotenv/config";
 import axios from "axios";
 
-const {NODE_ENV} = process.env.NODE_ENV;
-const URL = NODE_ENV === 'development' ? "http://localhost:3001/api" : "/api";
+//const {NODE_ENV} = process.env.NODE_ENV;
+//const URL = NODE_ENV === 'development' ? "http://localhost:3001/api" : "/api";
+const URL = "https://spotify-i2ay.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
